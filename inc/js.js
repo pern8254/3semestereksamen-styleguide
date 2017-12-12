635363d716957bf8ead32f4036fd190ac0385d39
pern8254 <33058628+pern8254@users.noreply.github.com>
2017-12-12 22:46:09 +0100

    Færdig style guide

--- inc/js.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {myFunction()};
-
-function myFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("nav").className = "background";
-    } else {
-        document.getElementById("nav").className = "";
-    }
-}
